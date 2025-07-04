b58f06c8dd2210235289b774569866b6fb8e34c3
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-07-04 11:56:43 +0530

    Update Db.js

--- DB/Db.js
+++ DB/Db.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    process.exit(1); // Stop app if DB connection fails
   }
 };
 
